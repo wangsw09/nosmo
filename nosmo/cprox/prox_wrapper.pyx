f3ad5e105acf48ac71ff431a63b8b84254bca65b
Shuaiwen Wang <sw2853@columbia.edu>
2019-04-09 17:17:27 -0400

    minor modification

--- nosmo/cprox/prox_wrapper.pyx
+++ nosmo/cprox/prox_wrapper.pyx
@@ -6,7 +6,7 @@ from .sorted_L1_prox cimport sorted_L1_prox
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def slope(np.ndarray[dtype=np.float64_t, ndim=1] x, np.ndarray[dtype=np.float64_t, ndim=1] theta, double tau):
+def slope_prox(np.ndarray[dtype=np.float64_t, ndim=1] x, np.ndarray[dtype=np.float64_t, ndim=1] theta, double tau):
     cdef int p = x.shape[0]
     sorted_L1_prox(p, &x[0], &theta[0], tau)
 
