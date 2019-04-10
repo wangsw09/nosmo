c9a29ee01f479637c3363cb3a712913ebaf26fa2
Shuaiwen Wang <sw2853@columbia.edu>
2019-04-10 01:25:27 -0400

    added a <k_fold_vec> function to process a sequence of tunings altogether

--- nosmo/k_fold.py
+++ nosmo/k_fold.py
@@ -9,11 +9,28 @@ def k_fold(y, X, k, lam, func, **kwargs):
     mask = np.ones(n, dtype=np.bool)
     sep = np.linspace(0, n, k + 1).astype(np.int32)
     err = 0.0
-    for i in xrange(k):
+    for i in range(k):
         mask[sep[i] : sep[i + 1]] = False
         beta = func(y=y[mask], X=X[mask, :], lam=lam, **kwargs)
         pred = np.dot(X[sep[i] : sep[i+1], :], beta)
         err += np.sum((y[sep[i] : sep[i+1]] - pred) ** 2)
+        mask[sep[i] : sep[i + 1]] = True
+    err /= n
+    return err
+
+def k_fold_vec(y, X, k, lams, func, **kwargs):
+    n, p = X.shape
+    idx = np.arange(n)
+    npr.shuffle(idx)
+
+    mask = np.ones(n, dtype=np.bool)
+    sep = np.linspace(0, n, k + 1).astype(np.int32)
+    err = np.zeros(lams.shape[0])
+    for i in range(k):
+        mask[sep[i] : sep[i + 1]] = False
+        beta = func(y=y[mask], X=X[mask, :], lams=lams, **kwargs)
+        pred = np.dot(X[sep[i] : sep[i+1], :], beta)
+        err += np.sum((y[sep[i] : sep[i+1], np.newaxis] - pred) ** 2, axis=0)
         mask[sep[i] : sep[i + 1]] = True
     err /= n
     return err
